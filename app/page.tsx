a4cdf9f6d67fde93061cdd64ae00a653b11db884
lmangall <l.mangallon@gmail.com>
2024-04-10 01:34:42 +0200

    update text

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,14 @@ import { MailSubscribe } from '../components/MailchimpSubscribe';
 
 const LoveLetterPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center p-5">
+    <div className="flex flex-col items-center justify-left h-screen text-center p-5">
       <div className="max-w-lg mx-auto">
         {/* Adding font-bold for bold text and mb-4 for margin bottom */}
-        <p className="font-bold">Why Lang Genie?</p>
+        <p className="font-bold">Why LangGenie?</p>
         <p className="text-3xl">🧞</p>
-        <p className="mb-4">Because it will leverage AI to help you be fluent in your target language.</p>
-        <p>Heya,</p>
-        <p>I am working on that at the moment,</p>
+        <p className="mb-4">LangGenie will be a collection of AI apps to help master fluency while having fun.</p>
+        <p className="font-bold"> Heya,</p>
+        <p>As you can see I am working on that at the moment,</p>
         <p>you can shoot me a mail at <a href="mailto:contact@frenchezleo.com" className="text-blue-500 hover:text-blue-700">contact@frenchezleo.com</a> </p>
         <p>or connect through the classic socials:</p>
         <p><a href="https://github.com/lmangall/" className="text-blue-500 hover:text-blue-700">GitHub</a>, <a href="https://www.instagram.com/french_chez_leo/" className="text-blue-500 hover:text-blue-700">Instagram</a>, and <a href="https://www.linkedin.com/in/l-mangallon/" className="text-blue-500 hover:text-blue-700">LinkedIn</a>.</p>
