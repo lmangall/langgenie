4c577b2b3c58cb0b86c536f59f01d73eb49a207a
lmangall <l.mangallon@gmail.com>
2024-04-09 17:09:00 +0200

    index minimal message

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Assuming you're using React Router for navigation
+import Link from 'next/link'; 
 
 const LoveLetterPage = () => {
   return (
@@ -7,7 +7,7 @@ const LoveLetterPage = () => {
       <div className="max-w-lg mx-auto">
         <p>Lang Genie uses AI to help you be fluent in your target language.</p>
         <p>Heya,</p>
-        <p>I am working on that at the moment, you can shoot me a mail at <a href="mailto:contact@frenchezleo.com" className="text-blue-500 hover:text-blue-700">contact@frenchezleo.com</a> or follow/reach out with the classic socials: my <a href="https://github.com/lmangall/" className="text-blue-500 hover:text-blue-700">GitHub</a>, <a href="https://www.instagram.com/french_chez_leo/" className="text-blue-500 hover:text-blue-700">Instagram</a>, and <a href="https://www.linkedin.com/in/l-mangallon/" className="text-blue-500 hover:text-blue-700">LinkedIn</a>. Moreover, if you have an interest in language learning you might want to sign up for the newsletter to get updates on my soon to come apps (fun ones like <Link to="/loveletter" className="text-blue-500 hover:text-blue-700">Lang Genie Love Letter</Link> and more practical ones to come).</p>
+        <p>I am working on that at the moment, you can shoot me a mail at <a href="mailto:contact@frenchezleo.com" className="text-blue-500 hover:text-blue-700">contact@frenchezleo.com</a> or follow/reach out with the classic socials: my <a href="https://github.com/lmangall/" className="text-blue-500 hover:text-blue-700">GitHub</a>, <a href="https://www.instagram.com/french_chez_leo/" className="text-blue-500 hover:text-blue-700">Instagram</a>, and <a href="https://www.linkedin.com/in/l-mangallon/" className="text-blue-500 hover:text-blue-700">LinkedIn</a>. Moreover, if you have an interest in language learning you might want to sign up for the newsletter to get updates on my soon to come apps (fun ones like <Link href="/loveletter"><a className="text-blue-500 hover:text-blue-700">Lang Genie Love Letter</a></Link> and more practical ones to come).</p>
       </div>
     </div>
   );
