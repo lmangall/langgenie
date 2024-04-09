9ec7c20b11a4580f40d07e09f674e43f4076a795
lmangall <l.mangallon@gmail.com>
2024-04-10 01:43:55 +0200

    reorganized msg

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,21 @@ import { MailSubscribe } from '../components/MailchimpSubscribe';
 
 const LoveLetterPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-left p-5">
+    <div className="flex flex-col items-center justify-center h-screen text-center p-5">
       <div className="max-w-lg mx-auto">
         {/* Adding font-bold for bold text and mb-4 for margin bottom */}
+        <p> Heya,</p>
+        <p className="mb-10">As you can see this part is still WIP</p>
         <p className="font-bold">Why LangGenie?</p>
         <p className="text-3xl">🧞</p>
         <p className="mb-4">LangGenie will be a collection of AI apps to help master fluency while having fun.</p>
-        <p> Heya,</p>
-        <p>As you can see I am working on that at the moment,</p>
+        <p> I am starting with Link href="/loveletter"><a className="text-blue-500 hover:text-blue-700">Loveletter</a></Link>, but more is coming</p>
+        <p className="mt-4">Stay on the loop !</p>
+        <MailSubscribe />
+
         <p>you can shoot me a mail at <a href="mailto:contact@frenchezleo.com" className="text-blue-500 hover:text-blue-700">contact@frenchezleo.com</a> </p>
         <p>or connect through the classic socials:</p>
         <p><a href="https://github.com/lmangall/" className="text-blue-500 hover:text-blue-700">GitHub</a>, <a href="https://www.instagram.com/french_chez_leo/" className="text-blue-500 hover:text-blue-700">Instagram</a>, and <a href="https://www.linkedin.com/in/l-mangallon/" className="text-blue-500 hover:text-blue-700">LinkedIn</a>.</p>
-        <p className="mt-4">And if you have an interest in language learning you might want to sign up for the newsletter to get updates on my soon to come apps (fun ones like <Link href="/loveletter"><a className="text-blue-500 hover:text-blue-700">Loveletter</a></Link> and more practical ones to come).</p>
-        <MailSubscribe />
      </div>
     </div>
   );
