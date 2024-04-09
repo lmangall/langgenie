2af55eae84aeef385a96069000b1b4d6b9a07c60
lmangall <l.mangallon@gmail.com>
2024-04-09 14:59:53 +0200

    edited next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/loveletter',
+        destination: 'https://loveletter-seven.vercel.app/',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
