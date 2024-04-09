a6a5377d906a20a0c4d4a638da4ecd7e4567bbca
lmangall <l.mangallon@gmail.com>
2024-04-10 01:21:51 +0200

    home message non-compilation solved

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,8 @@ const LoveLetterPage = () => {
         <p className="mb-4">Because it will leverage AI to help you be fluent in your target language.</p>
         <p>Heya,</p>
         <p>I am working on that at the moment,</p>
-        <p>you can shoot me a mail at <a href="mailto:contact@frenchezleo.com" className="text-blue-500 hover:text-blue-700">contact@frenchezleo.com</a> or connect through the classic socials:</p>
+        <p>you can shoot me a mail at <a href="mailto:contact@frenchezleo.com" className="text-blue-500 hover:text-blue-700">contact@frenchezleo.com</a> </p>
+        <p>or connect through the classic socials:</p>
         <p><a href="https://github.com/lmangall/" className="text-blue-500 hover:text-blue-700">GitHub</a>, <a href="https://www.instagram.com/french_chez_leo/" className="text-blue-500 hover:text-blue-700">Instagram</a>, and <a href="https://www.linkedin.com/in/l-mangallon/" className="text-blue-500 hover:text-blue-700">LinkedIn</a>.</p>
         <p className="mb-4">And if you have an interest in language learning you might want to sign up for the newsletter to get updates on my soon to come apps (fun ones like <Link href="/loveletter"><a className="text-blue-500 hover:text-blue-700">Lang Genie Love Letter</a></Link> and more practical ones to come).</p>
  </div>
