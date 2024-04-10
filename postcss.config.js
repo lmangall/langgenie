012805ef11f51db8dc9f607f237abf3683f32bd4
lmangall <l.mangallon@gmail.com>
2024-04-10 09:58:06 +0200

    Fixing the PostCSS Configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,9 @@
+// postcss.config.js
 module.exports = {
-  // Other webpack configurations like entry, output, etc., should be here as well
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader', // Injects CSS into the DOM
-          { loader: 'css-loader', options: { importLoaders: 1 } }, // Processes @import and url() like import/require()
-          'postcss-loader', // Process CSS with PostCSS
-        ],
-      },
-      // Other rules can go here
-    ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    // Add other PostCSS plugins by their package name as a key
+    // and an empty object or configuration object as value
   },
-  // Other configurations like plugins can go here
 };
